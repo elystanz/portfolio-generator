3a2e5ba7558b0021a45ca5c46783b7f4c431930c
elystanz <elysestanziale@gmail.com>
2022-04-06 17:08:20 -0500

    modularize code

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
+const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-    </head>
 
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">GitHub</a></h2>
-    </body>
-    </html>
-    `;
-};
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if (err) throw err;
 
-console.log(name, github);
-console.log(generatePage(name, github));
+    console.log("Portfolio complete! Checkout index.html to see the output!");
+});
